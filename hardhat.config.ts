0a284a211e5456fa4c8997addf672cf0eada2e99
startup-dreamer <s_kbharbhunja@me.iitr.ac.in>
2023-09-24 20:14:30 +0530

    updating hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,6 +26,47 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://mainnet.infura.io/v3/${
+          process.env.API_KEY ? process.env.API_KEY : ''
+        }`,
+        blockNumber: 14390000,
+      },
+      accounts: {
+        count: 50,
+      },
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${
+        process.env.API_KEY ? process.env.API_KEY : ''
+      }`, // Replace with the actual URL of the sepoli network
+      accounts: process.env.DEPLOYMENT_PRIVATE_KEY
+        ? [process.env.DEPLOYMENT_PRIVATE_KEY]
+        : [],
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${
+        process.env.API_KEY ? process.env.API_KEY : ''
+      }`, // Replace with the actual URL of the goerli network
+      accounts: process.env.DEPLOYMENT_PRIVATE_KEY
+        ? [process.env.DEPLOYMENT_PRIVATE_KEY]
+        : [],
+    },
+    mumbai: {
+      url: 'https://rpc.ankr.com/polygon_mumbai',
+      accounts: process.env.DEPLOYMENT_PRIVATE_KEY
+        ? [process.env.DEPLOYMENT_PRIVATE_KEY]
+        : [],
+    },
+    polygon: {
+      url: 'https://rpc-mainnet.maticvigil.com',
+      accounts: process.env.DEPLOYMENT_PRIVATE_KEY
+        ? [process.env.DEPLOYMENT_PRIVATE_KEY]
+        : [],
+    },
+  },
   paths: {
     sources: "./src", // Use ./src rather than ./contracts as Hardhat expects
     cache: "./cache_hardhat", // Use a different cache for Hardhat than Foundry
