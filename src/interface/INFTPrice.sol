27507535596733050e10e4398243240188aaa17a
startup-dreamer <s_kbharbhunja@me.iitr.ac.in>
2023-09-27 23:02:24 +0530

    fixed price interface

--- src/interface/INFTPrice.sol
+++ src/interface/INFTPrice.sol
@@ -6,50 +6,6 @@ pragma solidity 0.8.19;
  * @notice This interface provides functions to get the price of NFTs and ETH to USD conversion rate.
  */
 interface INFTPrice {
-    /**
-     * @dev Error indicating that the Azuki NFT does not exist.
-     */
-    error AzukiNFTDoesNotExist();
-
-    /**
-     * @dev Error indicating that the BAYC (Bored Ape Yacht Club) NFT does not exist.
-     */
-    error BaycNFTDoesNotExist();
-
-    /**
-     * @dev Error indicating that the CloneX NFT does not exist.
-     */
-    error ClonexNFTDoesNotExist();
-
-    /**
-     * @dev Error indicating that the CoolCats NFT does not exist.
-     */
-    error CoolcatsNFTDoesNotExist();
-
-    /**
-     * @dev Error indicating that the CryptoPunks NFT does not exist.
-     */
-    error CryptopanksNFTDoesNotExist();
-
-    /**
-     * @dev Error indicating that the Cryptoadz NFT does not exist.
-     */
-    error CryptodazNFTDoesNotExist();
-
-    /**
-     * @dev Error indicating that the Doodles NFT does not exist.
-     */
-    error DoodlesNFTDoesNotExist();
-
-    /**
-     * @dev Error indicating that the MAYC (Mutant Ape Yacht Club) NFT does not exist.
-     */
-    error MaycNFTDoesNotExist();
-
-    /**
-     * @dev Error indicating that the World of Woman NFT does not exist.
-     */
-    error WorldOfWomanNFTDoesNotExist();
 
     /**
      * @notice Get the price of an NFT.
